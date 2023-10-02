020bb5c028f482e9c5c39ade66e8ef649e2037b6
dant0421 <137190267+dant0421@users.noreply.github.com>
2023-10-02 02:32:51 -0500

    mp6 done

--- mp6/updateBoard.c
+++ mp6/updateBoard.c
@@ -12,7 +12,27 @@
  * Pay attention for the edge and corner cells, they have less neighbors.
  */
 
-int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col){
+int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col) {
+    int i,j;                                //initialize loop counter
+    int liveNeighbor = 0;           //initialize live neighbor cells and status = 0
+
+    for (i=row-1; i<=row+1; i++) {
+        //if row is within bounds of the board
+        if (i>=0 && i<boardRowSize) {
+            for (j=col-1; j<=col+1; j++) {
+                //if col is within bounds of the board
+                if (j>=0 && j<boardColSize) {
+                    //if current cell is not at (row,col)
+                    if (!(i==row && j==col)) {
+                        if (*(board+(i*boardColSize+j)) == 1) {
+                            liveNeighbor++;         //increment liveNeighbor count
+                        }
+                    }
+                }
+            } 
+        }
+    }
+    return liveNeighbor;
 }
 /*
  * Update the game board to the next step.
@@ -24,6 +44,33 @@ int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, i
  * Output: board is updated with new values for next step.
  */
 void updateBoard(int* board, int boardRowSize, int boardColSize) {
+    int currLiveNeighbor;
+    int tempBoard[boardColSize*boardRowSize];        //initialize temporary board 
+    int i,j;      //intilize loop counter for copying board to newBoard
+
+    for (i=0; i<boardColSize*boardRowSize; i++) {
+        tempBoard[i] = *(board+i);      //copy board to newBoard
+    }
+    for (i=0; i<boardRowSize; i++) { 
+        for (j=0; j<boardColSize; j++) {
+            //check live neighbor cells for every cell in tempBoard
+            currLiveNeighbor = countLiveNeighbor(tempBoard,boardRowSize, boardColSize, i, j);
+            //if current cell = 0, only becomes 1 if it has exactly 3 live neighbors
+            if (tempBoard[i*boardColSize+j] == 0) {
+                if (currLiveNeighbor == 3) {
+                    *(board+(i*boardColSize+j)) = 1;
+                }
+            }
+            //if current cell = 1 and 2 or 3 live neighbors exist, cell remains alive
+            else if (currLiveNeighbor == 2 || currLiveNeighbor == 3) {
+                *(board+(i*boardColSize+j)) = 1;
+            }
+            //otherwise cell dies
+            else {
+                *(board+(i*boardColSize+j)) = 0;
+            }
+        }
+    }
 }
 
 /*
@@ -38,9 +85,22 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
  * return 0 if the alive cells change for the next step.
  */ 
 int aliveStable(int* board, int boardRowSize, int boardColSize){
+    int i,j;                                                    //initialize loop counter
+    int nextBoard[boardRowSize*boardColSize];                   //initialize nextBoard to compare with board
+    
+    for (i=0; i<boardRowSize*boardColSize; i++) {
+        nextBoard[i] = *(board+i);                              //copy board to nextBoard
+    }
+
+    updateBoard(nextBoard, boardRowSize, boardColSize);     //updateBoard to nextBoard
+
+    for (i=0; i<boardRowSize; i++) {
+        for (j=0; j<boardColSize; j++) {
+            //return 0 if cell changes statea
+            if (nextBoard[i*boardRowSize+j] != *(board + (i*boardRowSize+j))) {
+                return 0;
+            }
+        }
+    }
+    return 1;
 }
-
-				
-				
-			
-
